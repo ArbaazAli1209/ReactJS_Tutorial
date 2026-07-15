72574fa953964d6e3bac9d9b7cf427c96c10ba2c
Arbaaz <arbaaz8158@gmail.com>
2026-07-15 14:51:09 +0530

    Components

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,7 @@
+function Card() {
+    return <div className="card">
+                <h1>Sarthak Sharma</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, id?</p>
+            </div>
+}
+export default Card;
